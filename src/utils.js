f771ef1619af23facb9fc8624d945dbe042c0b3f
nathan <ahrl_ahrl@naver.com>
2019-11-04 16:42:37 +0900

    delay 푸는중

--- src/utils.js
+++ src/utils.js
@@ -367,6 +367,8 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function (func, wait) {
+    // setTimeout(function() {
+    // },wait);
   };
 
   /**
